526ad3ac993784893dbe366cdc528a3b8eff3d04
Presajew <shannonmladd@gmail.com>
2022-03-06 22:35:11 -0700

    Adding enumerations for card values

--- src/objects/Card.py
+++ src/objects/Card.py
@@ -1,6 +1,10 @@
+from src.enums.CardSuitEnum import CardSuitEnum
+from src.enums.CardValueEnum import CardValueEnum
+
+
 class Card:
 
     # Creating a simple object that stores suit and value
     def __init__(self, suit, value):
-        self.suit = suit
-        self.value = value
+        self.suit = CardSuitEnum(suit)
+        self.value = CardValueEnum(value)
