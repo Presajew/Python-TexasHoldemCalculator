8089b30af0cde42f50c58af1a3dddbb6f99b9c6c
Presajew <shannonmladd@gmail.com>
2022-03-07 20:44:33 -0700

    added poker hand class; felt dumb and removed the 'one' card from my list ---> deck size is now 52 rather than 56...

--- src/objects/Deck.py
+++ src/objects/Deck.py
@@ -3,19 +3,21 @@ import random
 
 
 class Deck:
-    
     cards = []
 
-    # create a deck of cards 
-    # inits with 52 unique combinations
     def __init__(self):
         self.init_cards()
 
-
     def init_cards(self):
-        for suit in range(0,4):
-            for value in range(0,14):
+        ### Generate a base 52 card deck ###
+        for suit in range(1,5):
+            for value in range(1,14):
                 self.cards.append(Card(suit, value))
 
     def shuffle(self):
+        ### Randomize the order of all cards in deck ###
         random.shuffle(self.cards)
+    
+    def deal_one_card(self):
+        ### Remove the first element of the deck and return its value ###
+        return self.cards.pop(0)
